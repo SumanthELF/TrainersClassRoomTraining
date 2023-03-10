dc4b847d0a09766663db27dc4e33105e04b1fd02
Sumanth <Sumanth@TYSS-Sumanth>
2023-03-10 11:48:27 +0530

    Modified GithubDemo,java

--- TrainTheTrainers/src/UpdateMarch10/GithubDemo.java
+++ TrainTheTrainers/src/UpdateMarch10/GithubDemo.java
@@ -3,8 +3,7 @@ package UpdateMarch10;
 public class GithubDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Added new class and Package");
 	}
 
 }
